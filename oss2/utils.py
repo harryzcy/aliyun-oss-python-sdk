ca1ab427817a551780f2d631dec2700856679d91
Chongyi Zheng <harryzheng25@gmail.com>
2022-06-25 01:53:52 -0400

    Revert "refine FileLikeAdapter code"
    This reverts commit c9cde49c5e905f9f21e1371fef9fcb94f30e64e3.

--- oss2/utils.py
+++ oss2/utils.py
@@ -405,12 +405,7 @@ class _FileLikeAdapter(object):
         if self.read_all:
             raise StopIteration
 
-        content = self.read(_CHUNK_SIZE)
-
-        if content:
-            return content
-        else:
-            raise StopIteration
+        return self.read(_CHUNK_SIZE)
 
     def read(self, amt=None):
         offset_start = self.offset
@@ -421,6 +416,7 @@ class _FileLikeAdapter(object):
         if not content:
             self.read_all = True
             _invoke_progress_callback(self.progress_callback, self.offset, None)
+            return to_bytes('')
         else:
             _invoke_progress_callback(self.progress_callback, self.offset, None)
 
@@ -437,7 +433,7 @@ class _FileLikeAdapter(object):
             content = _invoke_cipher_callback(self.cipher_callback, content, real_discard)
 
             self.discard -= real_discard
-        return content
+            return content
 
     @property
     def crc(self):
