0ea94f00c3abc33f363fb93b86958d500bf57970
Chongyi Zheng <harryzheng25@gmail.com>
2022-06-25 01:53:52 -0400

    Revert "Support async fetch task."
    This reverts commit 43c5696c3487027b9d33ebc37543ff5f9e8adfbf.

--- oss2/exceptions.py
+++ oss2/exceptions.py
@@ -269,9 +269,6 @@ class SignatureDoesNotMatch(ServerError):
     status = 403
     code = 'SignatureDoesNotMatch'
 
-class ObjectAlreadyExists(ServerError):
-    status = 400
-    code = 'ObjectAlreadyExists'
 
 def make_exception(resp):
     status = resp.status
